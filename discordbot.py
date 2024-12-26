d057c0a92177cd93627b5cac314aeed54e1b508e
zer0thgear <deepsheepsleep@gmail.com>
2024-12-26 14:26:17 -0600

    Refactor to contain the client in a class

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,24 @@
-import os
-
 import discord
+#from discord import ui
 
 from settings import Settings
 
-intents = discord.Intents.default()
-intents.message_content = True
+class MyClient(discord.Client):
+    def __init__(self) -> None:
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(intents=intents)
 
-client = discord.Client(intents=intents)
+    async def on_ready(self):
+        print(f"Logged in as {client.user}!")
 
-@client.event
-async def on_ready():
-    print(f"Logged in as {client.user}!")
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        if client.user.mentioned_in(message):
+            await message.channel.send("Hello!")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+client = MyClient()
 
 client.run(Settings.DISCORD_API_KEY)
