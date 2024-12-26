1d42789ec9633098619081c2c0e5f88c2e1411d0
zer0thgear <deepsheepsleep@gmail.com>
2024-12-26 16:20:49 -0600

    Include bot id and guild id for testing purposes

--- settings.py
+++ settings.py
@@ -5,5 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
+    BOT_ID = os.getenv("BOT_ID")
     DISCORD_API_KEY = os.getenv("DISCORD_API_KEY")
+    SERVER_ID = os.getenv("SERVER_ID")
     VENICE_API_KEY = os.getenv("VENICE_API_KEY")
